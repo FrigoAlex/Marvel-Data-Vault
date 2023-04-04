6ee87e9154c48ba16c2c69ae78c731a5143236f3
FrigoAlex <alejo012903@hotmail.com>
2023-04-03 23:12:20 -0500

    🎨 Format the Bookmark Action file

--- src/redux/actions/bookmarkActions.ts
+++ src/redux/actions/bookmarkActions.ts
@@ -1,40 +1,31 @@
-import {
-  Bookmark
-} from "../../types/ReducerBookmark";
+import { Bookmark } from "../../types/ReducerBookmark";
 
 export const addBookmark = (payload: Bookmark, state: Bookmark[]) => {
-  const idSet = new Set(
-    state.map((bookmark: Bookmark) => bookmark.id)
-  );
+  const idSet = new Set(state.map((bookmark: Bookmark) => bookmark.id));
   if (idSet.has(payload.id)) {
     return state;
   }
   return [...state, payload];
-  
 };
 
 export const deleteBookmark = (payload: Bookmark, state: Bookmark[]) => {
-  
-  return state.filter((bookmark: Bookmark) => bookmark.id !== payload.id || bookmark.type !== payload.type);
+  return state.filter(
+    (bookmark: Bookmark) =>
+      bookmark.id !== payload.id || bookmark.type !== payload.type
+  );
 };
 
-
 export const hideBookmark = (payload: Bookmark, state: Bookmark[]) => {
-  const idSet = new Set(
-    state.map((bookmark: Bookmark) => bookmark.id)
-  );
+  const idSet = new Set(state.map((bookmark: Bookmark) => bookmark.id));
   if (idSet.has(payload.id)) {
     return state;
   }
-  return [...state, payload]; 
+  return [...state, payload];
 };
 
 export const showBookmark = (payload: Bookmark, state: Bookmark[]) => {
-  return state.filter((bookmark: Bookmark) => bookmark.id !== payload.id || bookmark.type !== payload.type);
+  return state.filter(
+    (bookmark: Bookmark) =>
+      bookmark.id !== payload.id || bookmark.type !== payload.type
+  );
 };
-
-
-
-
-
-
